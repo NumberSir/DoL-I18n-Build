2c8aef7a270ab626b97f0719c9be2a1cd2dbde75
LyokoJeremie <lucheng989898@gmail.com>
2023-10-15 05:43:02 +0800

    GameVersion

--- change_mod_version.js
+++ change_mod_version.js
@@ -1,4 +1,3 @@
-
 var fs = require('fs');
 
 console.log('process.argv', process.argv)
@@ -6,7 +5,12 @@ var bootJson = process.argv[2];
 
 var boot = JSON.parse(fs.readFileSync(bootJson, 'utf8'));
 var version = process.argv[3];
+var game_version = process.argv[4];
 
 boot.version = version;
+const gvd = boot.dependenceInfo.find(T => T.modName === 'GameVersion');
+if (gvd) {
+    gvd.version = `=${game_version}`;
+}
 
 fs.writeFileSync(bootJson, JSON.stringify(boot, null, 2), 'utf8');
